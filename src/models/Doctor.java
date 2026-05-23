74defb1fff064005b498ee788f56071d3d5f4de1
21000032540sanjaykumar <sanjaykumarqwas@gmail.com>
2026-05-23 22:20:32 +0530

    updated project

--- src/models/Doctor.java
+++ src/models/Doctor.java
@@ -1,4 +1,25 @@
 package models;
-class Doctor{
+class Doctor extends person{
+    private String specialization;
+
+    public Doctor(int id,String name, String   specialization){
+        super(id,name);
+        this.specialization=specialization;
+    }
+
+    public String getSpecicalization(String specialization){
+        return specialization;
+    }
+
+    public void treatDisease(Patient patient){
+        System.out.println("Dr"+name +"is treating patient for"+patient.getDisease());
+    }
+
+    @Override    
+    public void showDetails(){
+        System.out.println("Doctor id:"+ id);
+        System.out.println("Doctor name:" +name);
+        System.out.println("Doctor specialiation:"+specialization);
+    }
 
 };
